ccb26b6c9d5f32a43e1e44feec676f4d4d7df7e3
charlottetrupin <59920560+charlottetrupin@users.noreply.github.com>
2021-04-04 11:40:57 +0200

    Update plateforme.ml

--- src/plateforme.ml
+++ src/plateforme.ml
@@ -19,4 +19,18 @@ let create name x y =
   Collision_S.register e;
   Draw_S.register e;
   e
+ 
+let rec generatePlateforme nb x y =
+    match nb with
+    |0 -> let angle = Random.float Globals.pi  in
+          let rx = ((cos angle) *. 50.) +. x in 
+          let ry = ((sin angle) *. 50.) +. y in 
+           create ("plateforme" ^ string_of_int nb) rx ry
+    |_ -> let angle = Random.float Globals.pi  in
+          let rx = ((cos angle) *. 50.) +. x in 
+          let ry = ((sin angle) *. 50.) +. y in 
+           create ("plateforme" ^ string_of_int nb) rx ry; generatePlateforme (nb-1) rx ry
+  
+
+  
 let _is_plat e = Entity.Table.mem plat e
