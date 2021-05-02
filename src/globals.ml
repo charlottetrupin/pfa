8daa473602272337132a0d620c096ea1cc39481f
charlottetrupin <59920560+charlottetrupin@users.noreply.github.com>
2021-05-02 19:48:32 +0200

    Update globals.ml

--- src/globals.ml
+++ src/globals.ml
@@ -3,12 +3,10 @@ let canvas_height = 800
 let wall_thickness = 1
 
 let play_height = 3500
-
 let pi = 2.0 *. asin 1.0
 
-let init_img = Gfx.load_image "images/start.png"
 let bg_img = Gfx.load_image "images/background_01.png"
 let plat_img = Gfx.load_image "images/plateforme_01.png"
-let per_img = Gfx.load_image "images/livai.png"
+let per_img = Gfx.load_image "images/livai.png" 
 let end_img = Gfx.load_image "images/gameOver.png"
 let win_img = Gfx.load_image "images/youWin.png"
